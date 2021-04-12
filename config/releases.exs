98d2f4d1d6a546e24440acbb6b74caa2067bb566
technicalcapt <technicalcapt@gmail.com>
2021-04-12 22:53:39 +0700

    try set ssl to true

--- config/releases.exs
+++ config/releases.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :peta_stack_example, PetaStackExample.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
