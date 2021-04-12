3b958cc6276d988a64b186ef0926cfaafe12fc32
technicalcapt <technicalcapt@gmail.com>
2021-04-12 11:50:13 +0700

    add alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,9 +16,17 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 import topbar from "topbar"
+import "alpinejs"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  dom: {
+    onBeforeElUpdated(from, to){
+      if(from.__x){ window.Alpine.clone(from.__x, to) }
+    }
+  },
+})
 
 // Connect if there are any LiveViews on the page
 liveSocket.connect()
